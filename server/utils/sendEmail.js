da94451f474fc86833799b1a810d36a473fcae36
rkvalandas <valandasuramakrishna@gmail.com>
2024-12-13 12:28:20 +0530

    add email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,22 +2,24 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, html) => {
-    const transporter = nodemailer.createTransport({
-        service: "Gmail", // or any other service (like Outlook, Yahoo, etc.)
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-    const mailOptions = {
-        from: `"Price Pulse" <${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        html,
-    };
+  const mailOptions = {
+    from: `"Price Pulse" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  };
 
-    await transporter.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
