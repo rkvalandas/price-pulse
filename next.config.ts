25a05bbe84d560c78d9c7dbf6665f8c9dac1954b
rkvalandas <valandasuramakrishna@gmail.com>
2025-05-17 11:59:15 +0530

    configure for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: "dist",
   images: {
     unoptimized: true, // Required for static export
     remotePatterns: [
