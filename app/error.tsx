b2086770254af9eca6fb8ccd43b8810710558b81
rkvalandas <valandasuramakrishna@gmail.com>
2025-05-17 15:30:02 +0530

    fix build error

--- app/error.tsx
+++ app/error.tsx
@@ -7,6 +7,7 @@ import BrandedBackground from "./components/ui/BrandedBackground";
 // This error component redirects to the home page in case of any error
 export default function Error({
   error,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   reset,
 }: {
   error: Error & { digest?: string };
